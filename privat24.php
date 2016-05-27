b63e46d070e0443cfdc51d17a838d300da4d4148
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2016-05-27 14:37:15 +0300

    Fix module version to validator rules.

--- privat24.php
+++ privat24.php
@@ -39,7 +39,7 @@ class Privat24 extends PaymentModule
     public function __construct()
     {
         $this->name = 'privat24';
-        $this->version = '0.1';
+        $this->version = '1.0';
         $this->author = 'Soft Industry';
         $this->tab = 'payments_gateways';
         $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
