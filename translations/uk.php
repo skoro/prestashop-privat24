5e27b70854d4071c8593c9aa344e9ca36afcb248
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2016-05-30 12:34:16 +0300

    Ukrainian translations added.

---
+++ translations/uk.php
@@ -0,0 +1,19 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{privat24}prestashop>privat24_23ca197d0452e82b973212819d663e32'] = 'Приват24';
+$_MODULE['<{privat24}prestashop>privat24_b66cffdd06c6e18190a96632f50f9b05'] = 'Дозволяє використовувати платіжну систему Приват24';
+$_MODULE['<{privat24}prestashop>privat24_44d7b2036166452a10240ddf1d7e93b0'] = 'Бажаєте видалити цей модуль ?';
+$_MODULE['<{privat24}prestashop>privat24_d712c500f00a67b86757e49eeff07312'] = 'Будь ласка, налаштуйте свій мерчант аккаунт.';
+$_MODULE['<{privat24}prestashop>privat24_d0e4239d1a8b998739595a7f1e2404f0'] = 'Будь ласка введіть емайли через кому.';
+$_MODULE['<{privat24}prestashop>privat24_0c97c7af964682cba0117bc55a47c568'] = 'ID мерчанта було оновлено.';
+$_MODULE['<{privat24}prestashop>privat24_fe5d926454b6a8144efce13a44d019ba'] = 'Недійсне конфігураційне значення.';
+$_MODULE['<{privat24}prestashop>privat24_136d8768eaf31f6d502cf402187974b5'] = 'Налаштування Приват24';
+$_MODULE['<{privat24}prestashop>privat24_229a7ec501323b94db7ff3157a7623c9'] = 'ID мерчанта';
+$_MODULE['<{privat24}prestashop>privat24_315f3c78ea54c8dadb082acc328dd5bd'] = 'Пароль мерчанта';
+$_MODULE['<{privat24}prestashop>privat24_7f77e3d7178f0b1979d13c101a456450'] = 'Вмикнути відлагодження';
+$_MODULE['<{privat24}prestashop>privat24_a3418ce40785563cd409d2dcaff789cf'] = 'Надіслати листа коли платіж буде прийнято';
+$_MODULE['<{privat24}prestashop>privat24_9790b70b0772c3b35108155cf340f315'] = 'Емайли';
+$_MODULE['<{privat24}prestashop>privat24_c9cc8cce247e49bae79f15173ce97354'] = 'Зберегти';
+$_MODULE['<{privat24}prestashop>privat24_b6b287fcde753d63583c953ff29f3d17'] = 'Прийом платежів Приват24';
