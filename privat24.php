d5e3dd8bdc4bec59db60651426e0b259e3d0b5f9
Alexei Skorobogatko <a.skorobogatko@soft-industry.com>
2017-05-15 16:29:34 +0300

    0.2.1 version.

--- privat24.php
+++ privat24.php
@@ -42,7 +42,7 @@ class Privat24 extends PaymentModule
     public function __construct()
     {
         $this->name = 'privat24';
-        $this->version = '0.2.0';
+        $this->version = '0.2.1';
         $this->author = 'Soft Industry';
         $this->tab = 'payments_gateways';
         $this->ps_versions_compliancy = array('min' => '1.6', 'max' => '1.6.99.99');
